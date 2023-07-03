a0a8ccead4b63d1dfd3784eed7a3ebeaa3ee8a6a
tengkusyafiq <tengku.mohd.syafiq@gmail.com>
2023-07-03 14:39:59 +0800

    add TODO on sst

--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,20 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from "sst"
+import { NextjsSite } from "sst/constructs"
 
 export default {
   config(_input) {
     return {
-      name: "next-baseapp",
-      region: "us-east-1",
-    };
+      name: "next-baseapp", // TODO: change this to your app's name
+      region: "us-east-1", // TODO: change this to your preferred region
+    }
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site")
 
       stack.addOutputs({
         SiteUrl: site.url,
-      });
-    });
+      })
+    })
   },
-} satisfies SSTConfig;
+} satisfies SSTConfig
