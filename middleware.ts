c5d55e571eb2356242cde04487d35b7227ee488f
tengkusyafiq <tengku.mohd.syafiq@gmail.com>
2023-07-23 19:44:42 +0800

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -37,5 +37,5 @@ export async function middleware(request: NextRequest) {
   /** Adds ?lang={locale} for next-translate package */
   request.nextUrl.searchParams.set("lang", locale)
 
-  return NextResponse.rewrite(request.nextUrl)
+  return NextResponse.rewrite(new URL(`/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`, request.nextUrl))
 }
