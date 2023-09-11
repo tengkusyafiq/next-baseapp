050e99845193289106a4332b2a2ce8488d42042f
Tengku Syafiq <tengku.mohd.syafiq@gmail.com>
2023-09-11 17:52:40 +0800

    Revert "try using stock middleware from nextjs docs"
    This reverts commit 181449c924e9e84d7ffd5be5d26bb7c34c681f24.

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,41 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
+/** Regex to check if current path equals to a public file */
 const PUBLIC_FILE = /\.(.*)$/
 
-export async function middleware(req: NextRequest) {
-  if (
-    req.nextUrl.pathname.startsWith("/_next") ||
-    req.nextUrl.pathname.includes("/api/") ||
-    PUBLIC_FILE.test(req.nextUrl.pathname)
-  ) {
-    return
+/** Ignore non-page paths */
+const shouldProceed = (pathname: string) => {
+  if (pathname.startsWith("/_next") || pathname.includes("/api/") || PUBLIC_FILE.test(pathname)) {
+    return false
+  }
+  return true
+}
+
+export async function middleware(request: NextRequest) {
+  const { locale, pathname } = request.nextUrl
+
+  /** Ignore non-page paths */
+  if (!shouldProceed(pathname)) return
+
+  if (request.nextUrl.locale === "default") {
+    /** Get user's locale */
+    const storedLocale = request.cookies.get("NEXT_LOCALE")?.value
+
+    const response = NextResponse.redirect(new URL(`/${storedLocale || "en"}/${request.nextUrl.pathname}`, request.url))
+
+    /** Store default locale in user's cookies */
+    if (!storedLocale)
+      response.cookies.set("NEXT_LOCALE", "en", {
+        path: "/",
+      })
+
+    /** Redirect user to default locale */
+    return response
   }
 
-  if (req.nextUrl.locale === "default") {
-    const locale = req.cookies.get("NEXT_LOCALE")?.value || "en"
+  /** Adds ?lang={locale} for next-translate package */
+  request.nextUrl.searchParams.set("lang", locale)
 
-    return NextResponse.redirect(new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url))
-  }
+  return NextResponse.rewrite(request.nextUrl)
 }
